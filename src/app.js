0d629746d8fab7079cd7e9287be88020a66d76cd
Angel Mendez <oangel0590@gmail.com>
2023-03-13 20:25:45 -0300

    Fixing

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
-import express from "express";
-import handlebars from 'express-handlebars';
-import productsRouter from "./routes/productsRouter.js";
-import cartsRouter from "./routes/cartsRouter.js"
+const express = require("express");
+const session = require('express-session');
+const userRouter = require("./routes/user");
 const app = express();
-
- app.engine('handlebars', handlebars.engine());
-app.set('views', './views');
-app.set('view engine' , 'handlebars');
-
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended:true}));
 
-app.use('/api/products', productsRouter); 
-app.use('/api/carts', cartsRouter);  
+app.use(session({
+    secret: 'secretSession',
+    resave:false,
+    saveUninitialized: false
+}))
+
+app.use('/user', userRouter)
+
+
 
 const PORT = process.env.PORT || 8080 ;
 
-app.get("/", (req, res) =>{
-    res.send("Home");
-})
- 
+
+
+
+
+
 const server = app.listen(PORT , ()=>{
     console.log(`Server running on port: ${server.address().port}`)
 }); 
